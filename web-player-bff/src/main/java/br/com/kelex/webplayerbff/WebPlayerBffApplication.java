88eff0c1722ca2d753df5e93381574fa2fadea1b
Kelvin Romero <kelvinromero@gmail.com>
2023-10-04 21:30:04 -0300

    project structure

---
+++ web-player-bff/src/main/java/br/com/kelex/webplayerbff/WebPlayerBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.kelex.webplayerbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPlayerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPlayerBffApplication.class, args);
+	}
+
+}
---
+++ web-player-bff/src/test/java/br/com/kelex/webplayerbff/WebPlayerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kelex.webplayerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPlayerBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
