4fb884b1e0d2fd82e48265dcf36d78b1d200f97f
Kelvin Romero <kelvinromero@gmail.com>
2023-10-24 20:39:08 -0300

    feat: publishes message when episode is saved

--- admin/app/models/episode.rb
+++ admin/app/models/episode.rb
@@ -1,5 +1,20 @@
 class Episode < ApplicationRecord
+  after_save :publish_to_queue
+
   belongs_to :podcast
 
   validates :title, presence: true
+
+  private
+
+  def publish_to_queue
+    message = {
+      episode_id: id,
+      title: title,
+      podcast_id: podcast_id,
+      media_url: audio_file
+    }.to_json
+
+    BunnyClient.new.publish(message)
+  end
 end
--- admin/spec/models/episode_spec.rb
+++ admin/spec/models/episode_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Episode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:podcast) { create(:podcast) }
+
+  it "is valid with a title" do
+    episode = Episode.new(title: "Sample Episode", podcast: podcast)
+    expect(episode).to be_valid
+  end
+
+  it "is invalid without a title" do
+    episode = Episode.new(title: nil, podcast: podcast)
+    expect(episode).to be_invalid
+  end
+
+  it "calls the publish_to_queue method after save" do
+    episode = Episode.new(title: "Sample Episode", podcast: podcast)
+    expect(episode).to receive(:publish_to_queue)
+    episode.save
+  end
+
+  it "publishes a message to the BunnyClient after save" do
+    episode = Episode.new(title: "Sample Episode", podcast: podcast)
+    expect(BunnyClient).to receive(:new).and_return(bunny_client = double)
+    expect(bunny_client).to receive(:publish)
+
+    episode.save
+  end
+
+  it "belongs to a podcast" do
+    association = Episode.reflect_on_association(:podcast)
+    expect(association.macro).to eq :belongs_to
+  end
 end
