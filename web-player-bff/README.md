451c622107997d1ff8a849685a325dcdcb9b5254
Kelvin Romero <kelvinromero@gmail.com>
2023-11-26 09:25:00 -0300

    chore: instruments and dockerizes the bff

---
+++ web-player-bff/README.md
@@ -0,0 +1,5 @@
+# Web Player BFF
+
+## Requirements
+- Java 16
+- Gradle
--- web-player-bff/src/main/java/com/kelex/webplayerbff/EpisodeController.java
+++ web-player-bff/src/main/java/com/kelex/webplayerbff/EpisodeController.java
@@ -1,8 +1,12 @@
 package com.kelex.webplayerbff;
 
+import com.kelex.webplayerbff.entities.Episode;
+import com.kelex.webplayerbff.repositories.EpisodeRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -10,6 +14,7 @@ import java.util.List;
 public class EpisodeController
 {
     private final EpisodeRepository episodeRepository;
+    private static final Logger log = LoggerFactory.getLogger(EpisodeController.class);
 
     public EpisodeController(EpisodeRepository episodeRepository) {
         this.episodeRepository = episodeRepository;
@@ -18,18 +23,22 @@ public class EpisodeController
     @QueryMapping
     List<Episode> episodes()
     {
+        log.info("episodes");
         return episodeRepository.findAll();
     }
 
     @QueryMapping
     List<Episode> episodesByTitle(@Argument String title)
     {
+        log.info("episodesByTitle");
         return episodeRepository.findByTitle(title);
     }
 
     @QueryMapping
     List<Episode> findEpisodeByFuzzyTitle(@Argument String title)
     {
+        log.info("findEpisodeByFuzzyTitle");
         return episodeRepository.findByFuzzyTitle("*" + title + "*");
     }
 }
+
