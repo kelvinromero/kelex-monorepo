ca406adf460274a80a94d324effb0c53e743b7cb
Kelvin Romero <kelvinromero@gmail.com>
2023-11-30 16:01:12 -0300

    instrument fatapi

--- cortex/app/main.py
+++ cortex/app/main.py
@@ -1,20 +1,28 @@
-import json
-from typing import Union
-
-from fastapi import FastAPI, HTTPException
 from youtube_transcript_api import YouTubeTranscriptApi
+import logging
+from fastapi import HTTPException, status
+
+from fastapi import FastAPI
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"ping": "pong"}
+async def read_root():
+    return {"Hello": "World"}
 
+
+@app.get("/ping")
+async def health_check():
+    return "pong"
 
 @app.get("/transcript")
 async def get_transcript(video_id: str):
     try:
+        logger.info(f"Getting transcript for video {video_id}")
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
     except Exception as e:
         raise HTTPException(status_code=502, detail="Video not found")
