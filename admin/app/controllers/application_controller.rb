b63e9a5d758fe312006396867ea7998404c12878
Kelvin Romero <kelvinromero@gmail.com>
2023-12-18 21:07:20 -0300

    fix: remove fixed user

--- admin/app/controllers/application_controller.rb
+++ admin/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_user
 
   private
 
   def set_user
-    @user = User.find(5)
+    @user = current_user
   end
 end
